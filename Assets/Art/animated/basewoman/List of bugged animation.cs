9be0257ce01f8ba7562defdb1ca93dd353e640cc
Xenorius <36483929+Xenorius@users.noreply.github.com>
2018-03-29 18:34:25 +0200

    animation mouvement
    ajout des blendtrees movement et Idle de l'héroine. Mise en évidence des animations ne bouclant pas correctement

---
+++ Assets/Art/animated/basewoman/List of bugged animation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Liste des animations buggé a ce jour :
+// tenue1_0_walk_SE ; tenue1_0_walk_N
+// Liste des animations non buggé a ce jour : 
+// tenue1_0_walk_S ; tenue1_0_walk_NE ; tenue1_0_walk_E
+
+
+public class Listofbuggedanimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,20 @@ public class PlayerController : MonoBehaviour {
     private bool playerMoving;
     private float playerDirX;
     private float playerDirZ;
-    private Vector2 lastMove;
+    private Vector3 lastMove;
 
+    public GameObject heroine;
+    private Animator heroineAnim;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        //le script de mouvement appartien a l'objet "Heroine Position", qui ne contient pas d'animator
+        //On cherche donc la réference a l'objet fils "Heroine" qui le contient affin de pouvoir 
+        //donner les information necessaire a l'animator.
+        heroine = GameObject.Find("Heroine");
+        heroineAnim = heroine.GetComponent<Animator>();
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -26,12 +33,11 @@ public class PlayerController : MonoBehaviour {
         // Si un des axes est différent de 0, il y a mouvement !
         if (Input.GetAxisRaw("Horizontal") != 0f || Input.GetAxisRaw("Vertical") != 0f)
         {
-
             // On corrige les axes des boutons pour que ça corresponde à ce que voit le joueur, tout en restant sur l'axe
             // On utilise 0.7 pour les diagonales car 0.7² = 0.49, et 0.98 est suffisamment proche de 1 pour que ça n'ait pas d'importance.
             // Si on utilisait 1 le personnage irait super vite en diagonale. (on utilise des VECTEURS)
             if (Input.GetAxisRaw("Horizontal") > 0f) {
-                
+
                 if (Input.GetAxisRaw("Vertical") > 0f) {
                     playerDirZ = 0f;
                     playerDirX = 1f;
@@ -90,6 +96,13 @@ public class PlayerController : MonoBehaviour {
 
             playerMoving = true; // on indique qu'à cette frame, le joueur bouge
             lastMove = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")); // on enregistre le dernier mouvement effectué, on en aura besoin !
+
+
         }
+        heroineAnim.SetFloat("PlayerDirX", playerDirX);
+        heroineAnim.SetFloat("PlayerDirZ", playerDirZ);
+        heroineAnim.SetBool("PlayerMoving", playerMoving);
+        heroineAnim.SetFloat("LastMoveX", lastMove.x);
+        heroineAnim.SetFloat("LastMoveY", lastMove.z);
     }
 }
