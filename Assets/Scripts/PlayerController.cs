56c7de5637a56b675ea2d5c619d7046754dc7bc9
Xenorius <36483929+Xenorius@users.noreply.github.com>
2018-04-04 17:22:08 +0200

    fin animation  et colision
    1) J'ai trouver comment utilisé le miroire des animations affin d'avoir toute les animations, cependant, les animations ne sont pas fluide pour 2 d'entre elles
    2)j'ai ajouter une box colider et un rigid body a l'héroine, j'ai adapté le code de mouvement pour utiliser des vitesse plutot que des transformations, le rendu a malgrés tout des sacades

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour {
 
     public GameObject heroine;
     private Animator heroineAnim;
+    private SpriteRenderer heroineSprite;
+    private Rigidbody heroineRigidBody;
 
     // Use this for initialization
     void Start () {
@@ -20,7 +22,15 @@ public class PlayerController : MonoBehaviour {
         //donner les information necessaire a l'animator.
         heroine = GameObject.Find("Heroine");
         heroineAnim = heroine.GetComponent<Animator>();
-        
+        // On récupére aussi la réference du sprite Renderer pour gerer les flips lorsque le personage 
+        //fait face a l'est (on utilise pas de sprite face a l'est)
+        //Note: Pas besoin de changer les option du spriteRenderer pour IDLE puisqu'il suit toujours
+        //un mouvement qui done a l'option flipx la bonne valeur.
+        heroineSprite = heroine.GetComponent<SpriteRenderer>();
+
+
+        //On provoque le mouvement par force, on a donc besoin de la réference du "Rigid body"
+        heroineRigidBody = GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
@@ -37,59 +47,86 @@ public class PlayerController : MonoBehaviour {
             // On utilise 0.7 pour les diagonales car 0.7² = 0.49, et 0.98 est suffisamment proche de 1 pour que ça n'ait pas d'importance.
             // Si on utilisait 1 le personnage irait super vite en diagonale. (on utilise des VECTEURS)
             if (Input.GetAxisRaw("Horizontal") > 0f) {
-
+                
                 if (Input.GetAxisRaw("Vertical") > 0f) {
+                    //NE
                     playerDirZ = 0f;
                     playerDirX = 1f;
+                    //on utilise le miroire de l'animation NW
+                    heroineSprite.flipX = true;
                 }
                 else if (Input.GetAxisRaw("Vertical") < 0f) {
+                    //SE
                     playerDirZ = -1f;
                     playerDirX = 0f;
+                    heroineSprite.flipX = true;
+
                 }
-                
+
                 else
                 {
+                    //E
                     playerDirZ = -0.7f;
                     playerDirX = 0.7f;
+                    heroineSprite.flipX = true;
+
                 }
             }
             else if (Input.GetAxisRaw("Horizontal") < 0f)
             {
                 if (Input.GetAxisRaw("Vertical") > 0f)
                 {
+                    //NW
                     playerDirZ = 1f;
                     playerDirX = 0f;
+                    //On retire l'option miroir au cas ou l'animation precedente était miroire
+                    heroineSprite.flipX = false;
+
                 }
                 else if (Input.GetAxisRaw("Vertical") < 0f)
                 {
+                    //SW
                     playerDirZ = 0f;
                     playerDirX = -1f;
+                    heroineSprite.flipX = false;
+
                 }
-                
+
                 else
                 {
+                    //W
                     playerDirZ = 0.7f;
                     playerDirX = -0.7f;
+                    heroineSprite.flipX = false;
+
                 }
             }
             else {
                 //Monter consiste en une translation X/Z
                 if (Input.GetAxisRaw("Vertical") > 0f)
                 {
+                    //N
                     playerDirZ = 0.7f;
                     playerDirX = 0.7f;
+                    heroineSprite.flipX = false;
+
                 }
                 //descendre consiste en une translation -X/-Z
                 else if (Input.GetAxisRaw("Vertical") < 0f)
                 {
+                    //S
                     playerDirZ = -0.7f;
                     playerDirX = -0.7f;
+                    heroineSprite.flipX = false;
                 }
             }
 
 
-            // On applique un vecteur de translation correspondant exactement à la direction, multiplié par la vitesse. 
-            transform.Translate(new Vector3(playerDirX * moveSpeed * Time.deltaTime, 0, playerDirZ * moveSpeed * Time.deltaTime));
+            //deprecated: On applique un vecteur de translation correspondant exactement à la direction, multiplié par la vitesse. 
+            //transform.Translate(new Vector3(playerDirX * moveSpeed * Time.deltaTime, 0, playerDirZ * moveSpeed * Time.deltaTime));
+            //On applique une force correspondant exactement a la direction multiplié par la vitesse
+            heroineRigidBody.velocity = new Vector3(playerDirX * moveSpeed , 0, playerDirZ * moveSpeed);
+
             // Note : Comme update est appelé toutes les frames, et non toutes les X millisecondes, et que les frames peuvent varier en temps, 
             // on multiplie par le temps de la dernière frame pour déterminer de combien il faut se déplacer. (si la dernière frame a mis du temps, on se déplacera davantage)
             // cela normalise la vitesse/seconde. 
@@ -99,10 +136,14 @@ public class PlayerController : MonoBehaviour {
 
 
         }
+        else
+        {
+            heroineRigidBody.velocity = new Vector3(0, 0, 0);
+        }
         heroineAnim.SetFloat("PlayerDirX", playerDirX);
         heroineAnim.SetFloat("PlayerDirZ", playerDirZ);
         heroineAnim.SetBool("PlayerMoving", playerMoving);
         heroineAnim.SetFloat("LastMoveX", lastMove.x);
-        heroineAnim.SetFloat("LastMoveY", lastMove.z);
+        heroineAnim.SetFloat("LastMoveZ", lastMove.z);
     }
 }
